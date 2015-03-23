02ba87f33a0aee85e03f9bb9c32a0fdf0f9dac2c
apoliakov <apoliakov@paradigm4.com>
2015-03-23 16:15:15 -0400

    Update recreate_db.sh

--- kg_loader/recreate_db.sh
+++ kg_loader/recreate_db.sh
@@ -9,31 +9,31 @@ remove(KG_GENOTYPE);
 remove(KG_CHROMOSOME);
 
 create array KG_SAMPLE
-< sample :string >
+< sample :string not null>
 [ sample_id ];               --system will pick chunk size of 1M by default
 
 create array KG_CHROMOSOME
-< chromosome: string >
+< chromosome: string not null>
 [ chromosome_id ];
 
 create array KG_VARIANT
-<reference:string,
- alternate:string,
- id:string NULL DEFAULT null,
- qual:double NULL DEFAULT null,
- filter:string NULL DEFAULT null,
- info:string NULL DEFAULT null,
- ac:double NULL DEFAULT null,
- af:double NULL DEFAULT null> 
+<reference:string not null,
+ alternate:string not null,
+ id:string            null,
+ qual:double          null,
+ filter:string        null,
+ info:string          null,
+ ac:double            null,
+ af:double            null> 
 [chromosome_id=0:*,1,0,
  start        =0:*,10000000,0,  --gives us about 30K variants per chunk; many INFO fields contain long strings
  end          =0:*,10000000,0,
  alternate_id =0:19,20,0];
 
 create array KG_GENOTYPE
-<allele_1:bool NULL DEFAULT null,
- allele_2:bool NULL DEFAULT null,
- phase:bool NULL DEFAULT null> 
+<allele_1:bool        null,
+ allele_2:bool        null,
+ phase:bool           null> 
 [chromosome_id=0:*,1,0,
  start        =0:*,10000000,0,
  end          =0:*,10000000,0,
