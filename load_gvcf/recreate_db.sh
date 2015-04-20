09bfcdd8e6c03e04c07c831c8de9aee51dfa9780
apoliakov <apoliakov@paradigm4.com>
2015-04-20 18:10:30 -0400

    Update recreate_db.sh

--- load_gvcf/recreate_db.sh
+++ load_gvcf/recreate_db.sh
@@ -5,6 +5,16 @@ remove(GVCF_DATA);
 remove(GVCF_CHROMOSOME);
 remove(GVCF_SAMPLE);
 
+create array GVCF_SAMPLE
+< sample :string >
+[sample_id];
+
+create array GVCF_CHROMOSOME
+< chromosome :string not null> 
+[chromosome_id];
+
+store(build(GVCF_CHROMOSOME, '[(1),(2),(3),(4),(5),(6),(7),(8),(9),(10),(11),(12),(13),(14),(15),(16),(17),(18),(19),(20),(21),(22),(23),(X),(Y),(MT)]', true), GVCF_CHROMOSOME);
+
 create array GVCF_DATA
 <
   ref : string null,
@@ -21,17 +31,5 @@ create array GVCF_DATA
   sample_id     =0:*,50,0,
   start         =0:*,10000000,0,
   end           =0:*,10000000,0
-];
-
-create array GVCF_CHROMOSOME
-<	chromosome :string >
-[
-	chromosome_id =0:*,1,0 
-];
-
-create array GVCF_SAMPLE
-< sample :string >
-[
-  sample_id =0:*,50,0
 ];"
 
