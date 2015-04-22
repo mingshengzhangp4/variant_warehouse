cca071435f137760cf70983e24cdc9369bc38a80
apoliakov <apoliakov@paradigm4.com>
2015-04-22 00:36:31 -0400

    Update recreate_db.sh

--- load_1000g/recreate_db.sh
+++ load_1000g/recreate_db.sh
@@ -16,7 +16,7 @@ create array KG_CHROMOSOME
 < chromosome:string not null>
 [ chromosome_id ];
 
-store(build(KG_CHROMOSOME, '[(1),(2),(3),(4),(5),(6),(7),(8),(9),(10),(11),(12),(13),(14),(15),(16),(17),(18),(19),(20),(21),(22),(23),(X),(Y),(MT)]', true), KG_CHROMOSOME);
+store(build(KG_CHROMOSOME, '[(1),(2),(3),(4),(5),(6),(7),(8),(9),(10),(11),(12),(13),(14),(15),(16),(17),(18),(19),(20),(21),(22),(X),(Y),(MT)]', true), KG_CHROMOSOME);
 
 create array KG_VARIANT
 <reference:string not null,
