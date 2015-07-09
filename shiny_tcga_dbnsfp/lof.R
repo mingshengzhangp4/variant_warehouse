16c8e346c9f5bffd1bb1243f56fc3a4776c1567b
apoliakov <apoliakov@paradigm4.com>
2015-07-08 22:48:21 -0400

    Update lof.R

--- shiny_tcga_dbnsfp/lof.R
+++ shiny_tcga_dbnsfp/lof.R
@@ -39,7 +39,7 @@ get_lof_data = function( tumors     = c('BRCA'),
     gene_filter  = paste(sprintf("gene_symbol = '%s'", gene_list), collapse = " or ") 
   }
   clin_filter  = "true"
-  if (!missing(clin_regex))
+  if (!missing(clin_regex) && nchar(clin_regex) > 0)
   {
     clin_filter = sprintf("regex(value, '.*%s.*')", clin_regex)
   }
@@ -115,5 +115,86 @@ get_lof_data = function( tumors     = c('BRCA'),
     )
   names(res2) = c("variant", "tumor", "sample", "gene")
   res2 = unique(res2)
+  
   return(res2)
+}
+
+full_lof_query = function(tumors     = c('BRCA'),
+                          clin_regex,
+                          gene_list  = c('TP53', 'NOTCH1', 'NOTCH2', 'KRAS', 'NRAS', 'LRP1B', 'BRAF', 'ERBB4', 'PTEN', 'EGFR'),
+                          KG_AF_threshold           = 0.01,
+                          score_name                = "SIFT_converted_rankscore",
+                          score_threshold           = 0.551)
+{
+  alts   = get_lof_data(tumors, clin_regex, gene_list, KG_AF_threshold, score_name, score_threshold)
+  gene   = alts$gene
+  sample = alts$sample
+  tumor  = alts$tumor
+  num_rows    = nrow(alts)
+  num_tumors  = length(levels(tumor))
+  
+  unique_gene   = levels(gene)
+  unique_sample = levels(sample)
+  num_genes     = length(unique_gene)
+  num_samples   = length(unique_sample)
+  gene_id       = match(gene,  unique_gene) 
+  sample_id     = match(sample,unique_sample)
+  
+  reupload = as.scidb(data.frame(sample_id, gene_id), types=c("int64", "int64"))
+  reupload = bind(reupload, "mutated", "true")
+  reupload = redimension(reupload, sprintf("<mutated:bool> 
+                                           [gene_id=1:%i,100,0, sample_id=1:%i,100,0]", num_genes, num_samples))
+  reupload = scidb(sprintf("merge(%s, build(<x: bool> [gene_id=1:%i,100,0, sample_id=1:%i, 100, 0], false))", reupload@name, num_genes, num_samples))
+  reupload = scidbeval(reupload, temp=TRUE)
+  
+  triangle = scidbeval(scidb(
+    sprintf(
+      "filter(
+        cast(
+         aggregate(
+          apply(
+           cross_join(
+            %s as left,
+            %s as right,
+            left.sample_id,
+            right.sample_id
+           ),
+           mutated_both,    double(iif(left.mutated=true and right.mutated=true,  1.0, 0.0)),
+           mutated_left,    double(iif(left.mutated=true and right.mutated=false, 1.0, 0.0)),
+           mutated_right,   double(iif(left.mutated=false and right.mutated=true, 1.0, 0.0))
+          ),
+          sum(mutated_both)  as mutated_both,
+          sum(mutated_left)  as mutated_left,
+          sum(mutated_right) as mutated_right,
+          left.gene_id,
+          right.gene_id
+         ),
+         <mutated_both:double null, mutated_left:double null, mutated_right:double null> [gene_id_1, gene_id_2]
+        ),
+        gene_id_2 > gene_id_1
+      )", reupload@name, reupload@name)), temp=TRUE)
+  
+  fisher_result = iqdf(
+    scidb(sprintf(
+      "apply(
+        apply(
+         %s, 
+         not_mutated,
+         %i - (mutated_both + mutated_left + mutated_right)
+        ),
+        pval, fishertest_p_value(mutated_both, mutated_both+mutated_left, mutated_right+not_mutated, mutated_both+mutated_right, 'two.sided'),
+        estimate, fishertest_odds_ratio(mutated_both, mutated_both+mutated_left, mutated_right+not_mutated, mutated_both+mutated_right)
+       )",
+       triangle@name, num_samples
+      )), n=Inf
+    )
+  fisher_result = data.frame( gene_1 = unique_gene[fisher_result$gene_id_1],
+                              gene_2 = unique_gene[fisher_result$gene_id_2],
+                              mutated_both  = fisher_result$mutated_both,
+                              mutated_left  = fisher_result$mutated_left,
+                              mutated_right = fisher_result$mutated_right,
+                              not_mutated   = fisher_result$not_mutated,
+                              pval          = fisher_result$pval,
+                              estimate      = fisher_result$estimate)                          
+  result = list(alts, fisher_result)
 }
