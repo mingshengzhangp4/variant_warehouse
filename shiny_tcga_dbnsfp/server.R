87581c9b07345b5ff88f4a29f466bc46f31962b9
apoliakov <apoliakov@paradigm4.com>
2015-07-08 22:48:41 -0400

    Update server.R

--- shiny_tcga_dbnsfp/server.R
+++ shiny_tcga_dbnsfp/server.R
@@ -7,22 +7,23 @@
 # END_COPYRIGHT 
 
 library(shiny)
+library(ggvis)
 source("lof.R")
 
 shinyServer(function(input, output, session) {
   lof_data <- reactive({
     input$goButton
-    get_lof_data( isolate(input$tumor_list),
-                  isolate(input$clin_regex),
-                  isolate(input$gene_list),
-                  isolate(input$kg_af_threshold),
-                  isolate(input$score_type),
-                  isolate(input$score_threshold)
+    full_lof_query( isolate(input$tumor_list),
+                    isolate(input$clin_regex),
+                    isolate(input$gene_list),
+                    isolate(input$kg_af_threshold),
+                    isolate(input$score_type),
+                    isolate(input$score_threshold)
                   )
   })
   
   output$output_text <- renderUI({
-    res <- lof_data()
+    res <- data.frame(lof_data()[1])
     result = sprintf("
 Total alterations: %i, Samples: %i, Unique alterations: %i
 <br/>
@@ -34,7 +35,7 @@ Total alterations: %i, Samples: %i, Unique alterations: %i
   })
   
   output$main_plot <- renderPlot({
-    result= lof_data()
+    result= data.frame(lof_data()[1])
     if (input$plot_style == "sample / gene")
     {
       reduced = data.frame(table(result[c("sample","gene")]))
@@ -52,6 +53,32 @@ Total alterations: %i, Samples: %i, Unique alterations: %i
         ggplot(data = reduced) + theme_bw() + aes(x=gene, y=Freq, fill=Freq) + geom_bar(stat="identity", width=0.95)
       }
     }
+  })
+  
+  output$alterations_table <- renderDataTable({data.frame(lof_data()[1]) }, options = list(orderClasses = TRUE))
+  output$fisher_test_table <- renderDataTable({data.frame(lof_data()[2]) }, options = list(orderClasses = TRUE))
+  
+  g <- reactive({
+    x <- data.frame(lof_data()[2])
+    i <- x$estimate>0 & x$estimate<Inf
+    x <- x[i,]
+    mx <- max(abs(log(x$estimate)))
+    d <- data.frame(log_pvalue=-log(x$pval), log_odds=log(x$estimate))
+    d$index <- sprintf("%s:%s",x$gene_1,x$gene_2)
+    add_tooltip(
+      scale_numeric(
+        add_axis(
+          layer_points(ggvis(d, ~log_odds, ~log_pvalue, key:=~index)),
+          "y", title="- log(p value)"), "x", domain=c(-mx,mx)),
+      html=function(x)x$index)
+  })
+  bind_shiny(g, "ggvisPlot", "ggvis_ui")
+  
+  output$networkPlot <- renderSimpleNetwork({
+    p <- input$pval
+    a <- data.frame(lof_data()[2])
+    x <- a[a$pval < p, c(1,2,7)]
+    simpleNetwork(x[,1:2],width=400,height=700, fontSize=12)
   })
   
 })
