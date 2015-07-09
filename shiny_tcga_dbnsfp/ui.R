403709f3d235b9117e421e65b4d1845b3d47a6ef
apoliakov <apoliakov@paradigm4.com>
2015-07-08 22:49:00 -0400

    Update ui.R

--- shiny_tcga_dbnsfp/ui.R
+++ shiny_tcga_dbnsfp/ui.R
@@ -7,6 +7,9 @@
 # END_COPYRIGHT 
 
 source("lof.R")
+library(ggvis)
+library(networkD3)
+
 gene.symbol = iqdf(GENE$gene_symbol, n=Inf)$gene_symbol
 tumor.type = iqdf(TUMOR_TYPE$tumor_type_name, n=Inf)$tumor_type_name
 scores     = iqdf("project(filter(attributes(DBNSFP_V2p9_VARIANT), regex(name, '.*score')), name, type_id)")$name
@@ -22,7 +25,8 @@ shinyUI(fluidPage(
       selectInput('tumor_list', 'Tumor Types', tumor.type, multiple=TRUE, selectize=TRUE, 
                   selected=c("BRCA")),
       selectInput('gene_list',  'Genes',       gene.symbol, multiple=TRUE, selectize=TRUE, 
-                  selected=c("EGFR", "ERBB2", "PTEN", "MUC16", "NOTCH1", "KRAS", "BRAF", "TP53")),
+                  selected=c('TP53', 'CDH1', 'PIK3CA', 'ERBB2', 'NRAS', 'FLT1', 'FLT3', 'HER2', 'NOTCH1', 'NOTCH2', 'KRAS', 'NRAS', 'LRP1B', 'BRAF', 'ERBB4', 'PTEN', 'EGFR')
+      ),
       textInput  ('clin_regex', 'Clinical match'),
       sliderInput('kg_af_threshold', '1000G Max AF', min=0, max=1, step=0.01, value=0.1),
       selectInput('score_type',  'dbNSFP score',  scores, multiple=FALSE, selectize=TRUE, 
@@ -32,14 +36,28 @@ shinyUI(fluidPage(
       selectInput('plot_style', 'Plot style', c('tumor / gene', 'sample / gene'), selected = 'tumor / gene'),
       width=4
      ),
-    
      mainPanel(
-      htmlOutput("output_text"),
-      plotOutput(outputId = "main_plot", height = "700px"),
-      #htmlOutput("variantText"),
-      #plotOutput(outputId = "main_plot", height = "500px", width="1000px"),
-      width=8
-    )
+      tabsetPanel( 
+       id = "tabset",
+       tabPanel('Plot',
+        htmlOutput("output_text"),
+        plotOutput(outputId = "main_plot", height = "700px")
+       ),
+       tabPanel('Alterations Data',
+        dataTableOutput('alterations_table')
+       ),
+       tabPanel('Fisher Test Data',
+        dataTableOutput('fisher_test_table')
+       ),
+       tabPanel('Fisher Volcano',
+        ggvisOutput('ggvisPlot')
+       ),
+       tabPanel('Fisher Network',
+        simpleNetworkOutput('networkPlot'),
+        sliderInput('pval', label = 'p value threshold', min = 0.0005, max = 0.05, step = 0.0005, value = 0.001)
+       )),
+       width = 8
+     )
    )
   )
 )
