9b77873f57ed11a29d810080a63ef7b33004efce
mingsheng zhang <mzhang@paradigm4.com>
2015-12-22 15:35:25 -0500

    revise scripts for opaque array save, transfer and reload

--- load_tcga/scidbArray_save_load.sh
+++ load_tcga/scidbArray_save_load.sh
@@ -15,13 +15,19 @@ done
 ## zip ##
 
 cd $saved_path
-ls |grep ${DATE}|xargs -I {} tar -cvf - {}|pigz TCGA_opaque.tar.gz
+# ls |grep ${DATE}|xargs -I {} tar -cvf - {}|pigz TCGA_opaque.tar.gz
 
 # ## tar.gz file transfer ##
 
-[scidb@P4-NODE001 ~]$ scp scidb@54.174.6.43:~/tcga_save/TCGA_opaque.tar.gz ~/
-Warning: Permanently added '54.174.6.43' (RSA) to the list of known hosts.
-scidb@54.174.6.43's password:
+#[scidb@P4-NODE001 ~]$ scp scidb@54.174.6.43:~/tcga_save/TCGA_opaque.tar.gz ~/
+# Warning: Permanently added '54.174.6.43' (RSA) to the list of known hosts.
+# scidb@54.174.6.43's password:
+#
+
+
+## direct transfer of unzipped file ##
+[scidb@P4-NODE001 ~]$ nohup scp scidb@54.174.6.43:~/tcga_save/* ~/
+
 
 
 
@@ -32,11 +38,17 @@ scidb@54.174.6.43's password:
 [scidb@P4-NODE001 ~]$
    bash /home/scidb/variant_warehouse/load_tcga/array_init2load_saved_array.sh
 
-   # unzip and load the arrays #
-   tar -xzf TCGA_${DATE}_tar.gz
+#    # unzip and load the arrays #
+#    pigz -d TCGA_opaque_tar.gz 
+#    OR
+#    tar -xzf TCGA_opaque_tar.gz
 
-   ls|grep ${DATE}|xargs -I {} iquery -anq "load({}, 0, 'opaque')"
+# debug mode #
+ [scidb@P4-NODE001 ~]$
+  ls /home/scidb/mzhang/variant_warehouse/load_tcga/TCGA_opaque_save|grep ${DATE}|xargs -tp -I {} iquery -aq "load({}, '/home/scidb/mzhang/variant_warehouse/load_tcga/TCGA_opaque_save/{}', 0, 'opaque')"
  
+# batch mode #
 
-
-
+ [scidb@P4-NODE001 ~]$
+  ls /home/scidb/mzhang/variant_warehouse/load_tcga/TCGA_opaque_save|grep ${DATE}|xargs -I {} iquery -anq "load({}, '/home/scidb/mzhang/variant_warehouse/load_tcga/TCGA_opaque_save/{}', 0, 'opaque')"
+ 
