d293dfed711cfce2ace7f09b7a957ac9216c860f
Kriti Sen Sharma <ksen@paradigm4.com>
2016-01-19 15:37:23 -0500

    sam loader v0.1.1

--- load_sam/load_sam.sh
+++ load_sam/load_sam.sh
@@ -1,18 +1,19 @@
 
-#  samtools view /mnt/temp/GS000038929-ASM.normal.sort.bam
-#    |head -100000000 > ~/Paradigm4_labs/my_variant_warehouse/load_sam/cg.sam
-# sudo touch /home/scidb/mzhang/cp_file/cg.sam
-sudo sh -c 'samtools view /home/scidb/mzhang/cp_file/GS000038929-ASM.normal.sort.bam>/home/scidb/mzhang/cp_file/cg.sam'
+#  samtools view /home/scidb/mzhang/cp_file/GS000038929-ASM.normal.sort.bam|head -20000000 > /home/scidb/mzhang/paradigm4_lab/variant_warehouse/load_sam/cg.sam
+#  iquery -anq "remove(sam_tmp)" >/dev/null 2>&1
+#  iquery -anq "remove(sam_array)" >/dev/null 2>&1
+#  iquery -anq "store(aio_input('/home/scidb/mzhang/paradigm4_lab/variant_warehouse/load_sam/cg.sam', 'num_attributes=12'), sam_tmp)"
 
 
-iquery -anq "remove(sam_tmp)" >/dev/null 2>&1
+#  # read and write from nfs mounted dir
+#  sudo sh -c 'samtools view /home/scidb/mzhang/cp_file/GS000038929-ASM.normal.sort.bam>/home/scidb/mzhang/cp_file/cg.sam'
+#  iquery -anq "remove(sam_tmp)" >/dev/null 2>&1
+#  iquery -anq "remove(sam_array)" >/dev/null 2>&1
+#  iquery -anq "store(aio_input('/home/scidb/mzhang/cp_file/cg.sam', 'num_attributes=12'), sam_tmp)"
+
 iquery -anq "remove(sam_array)" >/dev/null 2>&1
 
-
-iquery -anq "store(aio_input('/home/scidb/mzhang/paradigm4_lab/variant_warehouse/load_sam/cg.sam', 'num_attributes=12'), sam_tmp)"
-
-
-iquery -anq "create array sam_array <qname:string,flag:int64 null,rname:string null,pos:int64 null,mapq:int64 null,cigr:string null,rnext:string null,pnext:int64 null,tlen:int64 null,seq:string null compression 'zlib',qual:string null compression 'zlib',rg:string null> [line_no=0:9999999,10000000,0]"
+iquery -aq "create array sam_array <qname:string,flag:int64 null,rname:string null,pos:int64 null,mapq:int64 null,cigr:string null,rnext:string null,pnext:int64 null,tlen:int64 null,seq:string null compression 'zlib',qual:string null compression 'zlib',rg:string null> [q_no=0:*,10000000,0]"
 
 
 
@@ -23,6 +24,7 @@ insert(
     substitute(
       project(
         apply(
+          -- unpack to get unique q_no
           unpack(
             sam_tmp,
             q_no),
